ccc028e28c58f5add066da0126dd42665dd197cd
AnkitaS1996 <ankitashedage2661@gmail.com>
2021-05-12 23:29:15 +0530

    Basic_Airthmatic_Operators_Program

---
+++ Basic Program/Airthmatic_Operators.js
@@ -0,0 +1,26 @@
+console.clear();
+
+var N1 = 10;
+var N2 = 45;
+var N3 = 100;
+
+//Addition 
+console.log("Addition Of " + ' ' + N1 + ' & ' + N2 + ' ' + 'is = ', N1 + N2);
+console.log("Addition Of " + ' ' + N1 + ' & ' + N2 + ' & ' + N3 + ' ' + 'is = ', N1 + N2 + N3);
+
+//Multiplication
+console.log("Multiplication  Of " + ' ' + N1 + ' & ' + N2 + ' ' + 'is = ', N1 * N2);
+console.log("Multiplication Of " + ' ' + N1 + ' & ' + N2 + ' & ' + N3 + ' ' + 'is = ', N1 * N2 * N3);
+
+//Substraction
+console.log("Substraction of " + ' ' + N1 + ' & ' + N1 + ' ' + 'is = ', N2 - N1);
+console.log("Substraction Of " + ' ' + N3 + ' & ' + N2 + ' & ' + N1 + ' ' + 'is = ', N3 - N2 - N1);
+
+//Division
+console.log("Division Of " + ' ' + N2 + ' & ' + N1 + ' ' + 'is = ', N1 / N2);
+
+//Modulus
+console.log("Division Of " + ' ' + N2 + ' & ' + N1 + ' ' + 'is = ', N2 % N1);
+
+
+
