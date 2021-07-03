0af49cbb0b523e6aa416597d6612bb8cd899f359
AnkitaS1996 <ankitashedage2661@gmail.com>
2021-07-03 23:09:26 +0530

    Javascript Variable Program

---
+++ Basic Program/Javascript_Variable.js
@@ -0,0 +1,20 @@
+console.clear();
+var FirstNum = 10;
+console.log(FirstNum);
+var SecondNum = 15;
+console.log(SecondNum);
+// Rules For naming js Variable
+// -Name Can contain letters,digits,-and $
+var MySubject1,math$
+
+//-Name Cannot Start with a digits
+var maths
+
+//-Name can start with an _ or $
+var _SubjectMarks, $RankInClass;
+
+//-Name are Case Sensitive
+var x,X;
+
+//-Names cannot be reserved keywords
+var varName;
