1832ee81473f797a5190000100eead09310c5c0b
AnkitaS1996 <ankitashedage2661@gmail.com>
2021-09-09 22:06:18 +0530

    Basic String Concept

---
+++ Basic Program/String.js
@@ -0,0 +1,19 @@
+console.log("\n****************==== JavaScript String ====****************\n");
+var FirstString = "I Like JavaScript"
+
+console.log(typeof(FirstString))
+
+var SecondString = 'We Will Learn Javascript Together'
+
+console.log(typeof(SecondString))
+
+//What if my string contains a double Quote
+var doubleQuotesString = "This is a \"Javascript\" string"
+console.log(doubleQuotesString)
+
+//what if my string contains a Single Quote
+var SingleQuotesString = 'This is a \'JavaScript\' string'
+console.log(SingleQuotesString);
+
+var doubleQuotesString2 = "This is a \'Javascript\' string"
+var SingleQuotesString2 = 'This is a \"Javascript\" string'
